196f59d130eef80d86867e01a46e9a605adf48e4
Kevin Andrey Herrera Silva <kahs_kevin@hotmail.com>
2020-07-29 16:18:18 -0500

    Bests Sellers

--- app/Controllers/Http/SaleController.js
+++ app/Controllers/Http/SaleController.js
@@ -64,7 +64,7 @@ class SaleController {
         const Provider = use("App/Models/Provider")
         const Inventory = use("App/Models/Inventory")
 
-        const orders = await Order.query().where('deliveryDate', '=', date).orderBy('priority', 'asc').fetch()
+        const orders = await Order.query().where('deliveryDate', '=', date).orderBy('priority', 'asc').fetch().orderBy('priority', 'asc').fetch()
 
         for (const order of orders.toJSON()) {
             //if(this.Format_date(order.deliveryDate)==date)
@@ -104,6 +104,65 @@ class SaleController {
             Rta.data.push(sorted_sales[i])
         }
         return Rta
+    }
+    async best({request,response})
+    {
+        const Rta={}
+        Rta.status=200
+        Rta.message="Productos más vendidos el día 1 de marzo."
+        Rta["data"]=[]
+        const sales={}
+        sales["data"]=[]
+
+        const date=request.input('deliveryDate')
+
+        const Order = use("App/Models/Order")
+        const OrdersProducts = use("App/Models/OrdersProducts")
+        const Product = use("App/Models/Product")
+        const Provider = use("App/Models/Provider")
+        const Inventory = use("App/Models/Inventory")
+
+        const orders = await Order.query().where('deliveryDate', '=', date).orderBy('priority', 'asc').fetch()
+
+        for (const order of orders.toJSON()) {
+            //if(this.Format_date(order.deliveryDate)==date)
+            if(order.deliveryDate==date)
+            {
+                
+                const ordersproducts= await OrdersProducts.query().where('order_id', '=', order.id).fetch()
+                for(const op of ordersproducts.toJSON())
+                {
+                    const product= await Product.find(op.product_id)
+                    var data={}
+                    data.product_id=op.product_id
+                    data.name=product.name
+                    data.quantity=op.quantity
+                    var index=this.Check_sales(sales.data,data.product_id)
+                    if(index==0)//Verificar si el producto ya existe en el informe de ventas de hoy, si es 0 significa que no está
+                    {
+                        sales.data.push(data)
+                        //sales.push(data)
+                        
+                    }else
+                    {
+                        sales.data[index].quantity=sales.data[index].quantity+op.quantity
+                        //sales[index].quantity=sales[index].quantity+op.quantity
+                    }
+                }
+                //Ya que se tiene el informe de ventas en el objeto sales se procede a ordenar
+                var sorted_sales=this.Sort_products(sales.data,"desc")
+            }
+            else
+            {
+                continue
+            }
+            
+        }
+        for(var i=0;i<3;i++)
+        {
+            Rta.data.push(sorted_sales[i])
+        }
+        return Rta        
     }
 
 }
